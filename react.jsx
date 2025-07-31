4dd4effeda5b08eedc20a858d9d642c60b262393
Parthiv2007 <pparthiv2007@gmail.com>
2025-07-31 19:22:32 +0530

    Update react.jsx

--- react.jsx
+++ react.jsx
@@ -18,7 +18,7 @@ function App() {
 
       </div>
       <div className="cards">
-        <Card title="Hello World 1" description="You need to enable JavaScript to run this app."image="src = https://images.unsplash.com/photo-1751795195789-8dab6693475d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D"/>
+        <Card title="Hello World 1" description="You need to enable JavaScript to run this app."/>
         <Card title="Hello World 2" description="This HTML file is a template."/>
         <Card title="Hello World 3" description=" You can add webfonts, meta tags, or analytics to this file."/>
         <Card title="Hello World 4" description="To begin the development, run `npm start` or `yarn start`."/>
